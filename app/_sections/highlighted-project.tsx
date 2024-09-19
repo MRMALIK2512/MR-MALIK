630abbec779abc98e1020f06f5d97d32d03187ad
MRMALIK2512 <tabishmalik529@gmail.com>
2024-09-20 03:17:15 +0530

    cpmmot

--- app/_sections/highlighted-project.tsx
+++ app/_sections/highlighted-project.tsx
@@ -1,9 +1,11 @@
 import { StudyNotionfeaturesList } from "@/public/projects/edtechplatform/featureslist";
 import { InvoicifyFeatureList } from "@/public/projects/invoicify/featureslist";
+import { FusionPadfeaturesList } from "@/public/projects/fusionpad/featureslist";
 import { Typography } from "@/ui/atoms";
 import { IconOpenInNew } from "@/ui/atoms/icons";
 import { ImagesCarousel, RegularSection } from "@/ui/components";
 import { FancySectionTitle } from "@/ui/components/FancySectionTitle";
+import { FaRegPlayCircle } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,11 +14,161 @@ export default function HighlightedProject() {
 
   return (
     <>
-    {/* PROJECT 1 */}
+
       <FancySectionTitle title="Persønał_prøjects" />
-      <RegularSection id="projects">
+
+      
+
+
+
+      {/* PROJECT 1 */}
+
+      <RegularSection>
+
       <Typography variant="h1" className="mb-8">
         #1 <br />
+        Invoicify
+      </Typography>
+        <div className="lg:w-[75%] max-w-[700px] mb-8 lg:mb-16">
+          <Typography>
+          About
+          Invoicify is a web-based <span className="text-white">invoicing application</span> designed to help businesses manage their invoices with ease.
+          The platform allows users to  <span className="text-white">create, send, and track invoices,</span> view payment history, and upload their business logo.
+          <span className="text-white">Google authentication</span> is also integrated to make logging in <span className="text-white"> quick and secure.</span>
+          The project is built on <span className="text-white">MERN stack</span>
+          </Typography>
+        </div>
+
+        <Typography variant="link" className="text-white mb-11 mt-3 bg-lk-pink px-4 py-2 rounded-xl ">
+          <Link href={INVOICIFY_LINK} target="_blank" className="flex gap-x-3 items-center ">
+            View Live
+            <FaRegPlayCircle />
+          </Link>
+        </Typography>
+
+        <Typography variant="link" className="text-white px-2 ">
+          <Link href={INVOICIFY_GITHUB} target="_blank">
+            Github Repo
+            <IconOpenInNew />
+          </Link>
+        </Typography>
+
+      </RegularSection>
+
+      <ImagesCarousel items={InvoicifyFeatureList} />
+
+      <RegularSection>
+        <Typography variant="h2" className="!text-white mb-4">
+         Features
+        </Typography>
+        <ul className="flex flex-col gap-6 mb-40">
+          <li>
+            <Typography>⎔ Easy invoice creation for quick and professional billing.</Typography>
+          </li>
+          <li>
+            <Typography>
+              ⎔ Efficient client management with stored details.
+            </Typography>
+          </li>
+          <li>
+            <Typography>⎔ Automated PDF downloads for easy sharing and printing.</Typography>
+          </li>
+          <li>
+            <Typography>⎔ Real-time insights into earnings and overdue payments.</Typography>
+          </li>
+          <li>
+            <Typography>
+              ⎔ Direct email notifications for seamless client communication.
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              ⎔ Customizable templates to personalize with logos and fields.
+            </Typography>
+          </li>
+          <li>
+            <Typography>⎔ Mobile-friendly access for managing invoices on any device.</Typography>
+          </li>
+        </ul>
+      </RegularSection>
+
+
+
+      
+      {/* PROJECT 2 */}
+
+      <RegularSection>
+
+      <Typography variant="h1" className="mb-8">
+        #2 <br />
+        FusionPad
+      </Typography>
+        <div className="lg:w-[75%] max-w-[700px] mb-8 lg:mb-16">
+          <Typography>
+          About
+          FusionPad is an innovative online <span className="text-white">web editor</span> designed for <span className="text-white">HTML, CSS, and JavaScript</span>  development.
+          With features like <span className="text-white">auto-saving, tag autoclosing, and jQuery support,</span>  coding becomes seamless and efficient. Additionally, FusionPad includes a powerful  <span className="text-white"> Markdown editor,</span>  enabling users to generate formatted README files effortlessly. 
+          The Project is built with  <span className="text-white"> React</span> and deployed on <span className="text-white">Vercel.</span> 
+          </Typography>
+        </div>
+
+        <Typography variant="link" className="text-white mb-11 mt-3 bg-lk-pink px-4 py-2 rounded-xl  ">
+          <Link href={FUSIONPAD_LINK} target="_blank" className="flex gap-x-3 items-center ">
+            View Live
+            <FaRegPlayCircle />
+          </Link>
+        </Typography>
+
+        <Typography variant="link" className="text-white px-2`">
+          <Link href={FUSIONPAD_GITHUB} target="_blank">
+            Github Repo
+            <IconOpenInNew />
+          </Link>
+        </Typography>
+      </RegularSection>
+
+      <ImagesCarousel items={FusionPadfeaturesList} />
+
+      <RegularSection>
+        <Typography variant="h2" className="!text-white mb-4">
+         Features
+        </Typography>
+        <ul className="flex flex-col gap-6 mb-40">
+          <li>
+            <Typography>⎔ Real-Time Preview for instant code visibility.</Typography>
+          </li>
+          <li>
+            <Typography>
+              ⎔ User-Friendly Markdown Editor for easy README creation.
+            </Typography>
+          </li>
+          <li>
+            <Typography>⎔ Auto-Save to prevent data loss.</Typography>
+          </li>
+          <li>
+            <Typography>⎔ Autoclosing Tags and Brackets to reduce errors.</Typography>
+          </li>
+          <li>
+            <Typography>
+              ⎔ jQuery Support for enhanced interactivity.
+            </Typography>
+          </li>
+          <li>
+            <Typography>
+              ⎔ Responsive Design for a polished look on all devices.
+            </Typography>
+          </li>
+          <li>
+            <Typography>⎔ Downloadable README Files for convenient documentation.</Typography>
+          </li>
+        </ul>
+      </RegularSection>
+
+            
+      {/* PROJECT 3 */}
+      <RegularSection id="projects">
+      <Typography variant="h1" className="mb-8">
+        #3 <br />
         Study
         Notion
       </Typography>
@@ -75,70 +227,6 @@ export default function HighlightedProject() {
           </li>
         </ul>
       </RegularSection>
-
-      <RegularSection>
-
-      <Typography variant="h1" className="mb-8">
-        #2 <br />
-        Invoicify
-      </Typography>
-        <div className="lg:w-[75%] max-w-[700px] mb-8 lg:mb-16">
-          <Typography>
-          About
-          Invoicify is a web-based <span className="text-white">invoicing application</span> designed to help businesses manage their invoices with ease.
-          The platform allows users to  <span className="text-white">create, send, and track invoices,</span> view payment history, and upload their business logo.
-          <span className="text-white">Google authentication</span> is also integrated to make logging in <span className="text-white"> quick and secure.</span>
-          The project is built on <span className="text-white">MERN stack</span>
-          </Typography>
-        </div>
-
-        <Typography variant="link" className="text-white">
-          <Link href={INVOICIFY_GITHUB} target="_blank">
-            Github Repo
-            <IconOpenInNew />
-          </Link>
-        </Typography>
-      </RegularSection>
-
-      <ImagesCarousel items={InvoicifyFeatureList} />
-
-      <RegularSection>
-        <Typography variant="h2" className="!text-white mb-4">
-         Features
-        </Typography>
-        <ul className="flex flex-col gap-6 mb-40">
-          <li>
-            <Typography>⎔ Easy invoice creation for quick and professional billing.</Typography>
-          </li>
-          <li>
-            <Typography>
-              ⎔ Efficient client management with stored details.
-            </Typography>
-          </li>
-          <li>
-            <Typography>⎔ Automated PDF downloads for easy sharing and printing.</Typography>
-          </li>
-          <li>
-            <Typography>⎔ Real-time insights into earnings and overdue payments.</Typography>
-          </li>
-          <li>
-            <Typography>
-              ⎔ Direct email notifications for seamless client communication.
-            </Typography>
-          </li>
-          <li>
-            <Typography>
-              ⎔ Customizable templates to personalize with logos and fields.
-            </Typography>
-          </li>
-          <li>
-            <Typography>⎔ Mobile-friendly access for managing invoices on any device.</Typography>
-          </li>
-        </ul>
-      </RegularSection>
-
-
- 
     </>
   );
 }
@@ -147,3 +235,13 @@ const STUDY_NOTION_GITHUB =
   "https://github.com/MRMALIK2512/ED-techplatform";
 const INVOICIFY_GITHUB =
   "https://github.com/MRMALIK2512/Invoicify";
+
+const FUSIONPAD_GITHUB =
+  "https://github.com/MRMALIK2512/fusionpad";
+
+const INVOICIFY_LINK =
+  "https://invoicify-seven.vercel.app/";
+
+const FUSIONPAD_LINK =
+  "https://fusionpad.vercel.app/";
+
---
+++ public/projects/fusionpad/featureslist.tsx
@@ -0,0 +1,24 @@
+export const FusionPadfeaturesList: {
+    id: string;
+    imageSrc: string;
+    label: string;
+  }[] = [
+    {
+      id: "fi-001",
+      label: "Feature 1",
+      imageSrc: "/projects/fusionpad/homepage.png",
+    },
+    {
+      id: "fi-002",
+      label: "Feature 2",
+      imageSrc: "/projects/fusionpad/webeditor.png",
+    },
+    {
+      id: "fi-003",
+      label: "Feature 3",
+      imageSrc: "/projects/fusionpad/markdowneditor.png",
+    },
+    
+  ];
+  
+  
